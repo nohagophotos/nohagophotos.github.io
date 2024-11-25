d7c28b2e74b4f7a1a2833abfbca1c100f69f64a0
Ugart3e <101062817+Ugart3e@users.noreply.github.com>
2024-11-25 19:47:48 +0100

    rebuild?

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import p11 from '../img/IMG_0088.jpg'
 
 
 const contacto = () => {
    return (
        <>
            <Navbar/>
-           <div className='w-9/12 h-96 m-auto mt-6 flex'>
+           <div className='w-9/12 h-96 m-auto mt-6 flex mb-64'>
            <div className='border-2 border-black w-6/12 h-auto m-auto flex flex-col'>
            <p>instagram: nhpproductions</p>
            <p>gmail: nohagophotos@gmail.com</p>
@@ -16,7 +17,8 @@ const contacto = () => {
            <div className='border-2 border-black h-12 mt-4'></div>
            <div className='border-2 border-black h-36 mt-4'></div>
            </div>
-           <div className='border-2 border-black w-6/12 h-full m-auto flex flex-col'>
+           <div className='border-2 border-black w-4/12 m-auto flex flex-col'>
+           <img src={p11} className='object-cover w-full h-full '/>
            </div>
            </div>
            <Footer/>
